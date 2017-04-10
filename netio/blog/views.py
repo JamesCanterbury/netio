86962481df0ca4560433c755b68ce8feeff51ab1
James Canterbury <James.Canterbury@gmail.com>
2017-04-10 21:15:11 +0000

    Added view and template for detailed blog post as well as css for the site.

--- netio/blog/views.py
+++ netio/blog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 def post_list(request):	
-	posts = Post.objects.filter()
+	posts=Post.objects.filter()
 	return render(request, 'blog/post_list.html', {'posts' : posts})
+def post_detail(request, pk):
+	Post.objects.get(pk=pk)
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+	#Old Way   -posts = Post.objects.filter()
+	## old way -return render(request, 'blog/post_list.html', {'posts' : posts})
+	#posts = Post.objects.filter(published_date=timezone.now()).order_by('published_date')
